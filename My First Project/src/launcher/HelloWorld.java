96586a01f40fcc99a127b47ac5126f273098e294
Hanchen Liu <772185085@qq.com>
2018-01-03 13:42:31 -0800

    Initial Project for IT 333

---
+++ My First Project/src/launcher/HelloWorld.java
@@ -0,0 +1,10 @@
+package launcher;
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My name is Hanchen Liu");
+        System.out.println("I have used the following IDEs: Komodo, Eclipse, Visual Studio and JGrasp!");
+    }
+}
