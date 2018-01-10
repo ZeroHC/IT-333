cde696abbbc977687548e2bbc9fcd5d783418506
Hanchen Liu <772185085@qq.com>
2018-01-10 14:13:07 -0800

    Initial Files for assignment 1

---
+++ Zero's Assignment 1 Stacks & Queues Part #1/src/exceptions/StackUnderflowException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class StackUnderflowException extends RuntimeException
+{
+    public StackUnderflowException(String message)
+    {
+        super(message);
+    }
+}
---
+++ Zero's Assignment 1 Stacks & Queues Part #1/src/structures/Stack.java
@@ -0,0 +1,55 @@
+package structures;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Stack<T> implements IStack<T>
+{
+    @Override
+    public void push(T element)
+    {
+
+    }
+
+    @Override
+    public void pushAll(T[] elements)
+    {
+
+    }
+
+    @Override
+    public T pop()
+    {
+        return null;
+    }
+
+    @Override
+    public List<T> popAll()
+    {
+        return null;
+    }
+
+    @Override
+    public int size()
+    {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator()
+    {
+        return null;
+    }
+
+    @Override
+    public void clear()
+    {
+
+    }
+}
