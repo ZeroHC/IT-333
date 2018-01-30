739ef0e58b4b6299adf5fe74938d906ce76d8d79
ZeroHC <772185085@qq.com>
2018-01-29 17:18:05 -0800

    initial files for assignment 2

---
+++ Zero's Assignment 2 Stacks & Queues Part #2/src/structures/TwoWayQueue.java
@@ -0,0 +1,74 @@
+package structures;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class TwoWayQueue<T> implements ITwoWayQueue
+{
+
+    @Override
+    public Object dequeueFirst()
+    {
+        return null;
+    }
+
+    @Override
+    public Object dequeueLast()
+    {
+        return null;
+    }
+
+    @Override
+    public List dequeueAll()
+    {
+        return null;
+    }
+
+    @Override
+    public void enqueueFirst(Object element)
+    {
+
+    }
+
+    @Override
+    public void enqueueLast(Object element)
+    {
+
+    }
+
+    @Override
+    public void enqueueAllFirst(Object[] elements)
+    {
+
+    }
+
+    @Override
+    public void enqueueAllLast(Object[] elements)
+    {
+
+    }
+
+    @Override
+    public int size()
+    {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        return false;
+    }
+
+    @Override
+    public void clear()
+    {
+
+    }
+
+    @Override
+    public Iterator iterator()
+    {
+        return null;
+    }
+}
